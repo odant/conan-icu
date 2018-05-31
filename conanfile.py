9fe60e34a49fd2c7030f505f551ab416216aa113
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-05-31 12:15:28 +0300

    Revert package_info (use collect_libs)

--- conanfile.py
+++ conanfile.py
@@ -134,7 +134,7 @@ class ICUConan(ConanFile):
     def package_info(self):
         self.cpp_info.defines = ["U_DISABLE_RENAMING=1"]
         if self.settings.os == "Windows":
-            self.cpp_info.libs = ["icuio", "icuin", "icuuc", "icudt"]
+            self.cpp_info.libs = tools.collect_libs(self)
         else:
             self.cpp_info.libs = ["icuio", "icui18n", "icuuc", "icudata"]
 
