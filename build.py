30514f1af548a40b4766901120817209353d2d89
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-08-03 13:19:01 +0300

    Simplification

--- build.py
+++ build.py
@@ -12,32 +12,7 @@ username = "odant" if "CONAN_USERNAME" not in os.environ else None
 # Windows settings
 visual_versions = ["14", "15"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
 visual_runtimes = ["MD", "MDd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
-visual_default_toolsets = {
-    "14": [None, "v140_xp"],
-    "15": [None, "v141_xp"]
-}
-visual_toolsets = None
-if "CONAN_VISUAL_TOOLSETS" in os.environ:
-    visual_toolsets = [s.strip() for s in os.environ["CONAN_VISUAL_TOOLSETS"].split(",")]
 dll_sign = False if "CONAN_DISABLE_DLL_SIGN" in os.environ else True
-
-
-def vs_get_toolsets(compiler_version):
-    return visual_toolsets if not visual_toolsets is None else visual_default_toolsets.get(compiler_version)
-
-
-def vs_add_toolset(builds):
-    result = []
-    for settings, options, env_vars, build_requires, reference in builds:
-        toolsets = vs_get_toolsets(settings["compiler.version"])
-        if toolsets is None or settings["compiler"] != "Visual Studio":
-            result.append([settings, options, env_vars, build_requires, reference])
-        else:
-            for t in toolsets:
-                settings = deepcopy(settings)
-                settings["compiler.toolset"] = t
-                result.append([settings, options, env_vars, build_requires, reference])
-    return result
 
 
 def add_dll_sign(builds):
@@ -68,7 +43,6 @@ if __name__ == "__main__":
     # Adjusting build configurations
     builds = builder.items
     if platform.system() == "Windows":
-        builds = vs_add_toolset(builds)
         builds = add_dll_sign(builds)
     if platform.system() == "Linux":
         builds = filter_libcxx(builds)
