6b871b010dc785f7ade4ac8fed968666ab8182c3
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-08-07 19:09:06 +0300

    Update test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,12 +3,12 @@
 
 
 from conans import ConanFile, CMake, tools
-import os
 
 
 class PackageTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    build_requires = "ninja/1.9.0"
 
     def imports(self):
         self.copy("*.pdb", dst="bin", src="bin")
@@ -16,13 +16,14 @@ class PackageTestConan(ConanFile):
         self.copy("*.so*", dst="bin", src="lib")
 
     def build(self):
-        cmake = CMake(self)
+        cmake = CMake(self, generator="Ninja", msbuild_verbosity='normal')
         cmake.verbose = True
         cmake.configure()
         cmake.build()
+        self.cmake_is_multi_configuration = cmake.is_multi_configuration
 
     def test(self):
-        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+        if self.cmake_is_multi_configuration:
             self.run("ctest --verbose --build-config %s" % self.settings.build_type)
         else:
             self.run("ctest --verbose")
