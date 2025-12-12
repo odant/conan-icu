d5836da43dd3c56ded362da9046ae6a3b570e4c0
d0nc1h0t <udincev@yandex.ru>
2025-12-12 12:18:26 +0300

    Version 78.1+0

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ import os, glob, shutil, platform
 
 class ICUConan(ConanFile):
     name = "icu"
-    version = "77.1+1"
+    version = "78.1+0"
     license = "http://www.unicode.org/copyright.html#License"
     description = "ICU is a mature, widely used set of C/C++ and Java libraries " \
                   "providing Unicode and Globalization support for software applications."
